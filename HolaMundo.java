c0783aca7ad432792c48c14727b0f267e7d9f77e
andymonty83 <a.montagut@hotmail.es>
2026-07-19 19:24:28 +0200

    sincro a la fuerza

--- HolaMundo.java
+++
@@ -1,9 +0,0 @@
-package holamundo;
-
-public class HolaMundo {
-    public static void main(String[] args) throws Exception {
-
-        System.out.println("Hola Mundo!!! Bienvenidos a TODOCode!!!");
-
-    }
-}
